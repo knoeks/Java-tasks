b18620aa99e1152247a8b8c9f3b73606bce26d23
Paul <povilas.geguzis@gmail.com>
2025-01-17 11:22:08 +0400

    s

--- 13_praktiniai_stream/13_02.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ 13_praktiniai_stream/13_02.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -1,33 +1,35 @@
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.io.File;
-import java.io.FileWriter;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class ReadingFilesPerLine {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     // test the method here
-    List<String> lines = read("text.txt");
+    System.out.println("file path: ");
+    List<String> lines = read(scanner.nextLine());
     lines.stream().forEach(System.out::println);
   }
 
   public static List<String> read(String file) {
     List<String> books = new ArrayList<>();
     try {
-      File file1 = new File(file);
-      Scanner myReader = new Scanner(file1);
-      while (myReader.hasNextLine()) {
-        books.add(myReader.nextLine());
-      }
-    } catch (FileNotFoundException e) {
+//      File file1 = new File(file);
+//      Scanner myReader = new Scanner(file1);
+//      while (myReader.hasNextLine()) {
+//        books.add(myReader.nextLine());
+//      }
+
+      books = Files.lines(Paths.get(file))
+              .sorted().collect(Collectors.toList());
+    } catch (IOException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
